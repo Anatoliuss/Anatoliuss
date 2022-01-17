5dedc4e9755a40a89821eec0bbc158f58947a970
Anatoliuss <achuvashlov@inbox.ru>
2022-01-17 23:23:58 +0300

    Первая попытка

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("У меня не сохранились предыдущие задания, а со скриншота как-то неудобно копировать... Надеюсь, так тоже можно");
+    }
+
+}
